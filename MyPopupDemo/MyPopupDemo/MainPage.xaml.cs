0823ad495edec81f47373fa8084a7a762198941f
sumitsisodia.xamarinskills@gmail.com <sumitsisodia@elensoft.com>
2018-01-11 20:48:23 +0530

    Add project files.

---
+++ MyPopupDemo/MyPopupDemo/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyPopupDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.PushAsync(new ShowPopupDemo());
+        }
+    }
+}
---
+++ MyPopupDemo/MyPopupDemo/ShowPopupDemo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyPopupDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowPopupDemo : PopupPage
+    {
+        public ShowPopupDemo()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.PopAsync(true);
+        }
+    }
+}
